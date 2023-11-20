2339a2437f608d2fdf7f221ebcf938895cede661
bidsouravbest <souravbid321@gmail.com>
2023-11-20 18:04:52 +0530

    abc

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import data from "./UserDetailsJson.json";
 
 const Users = () => {
+  const [searchParams, setSearchParams] = useSearchParams({ filter: "disb" });
+  const status = searchParams.get("filter");
+  const message =
+    status !== "active"
+      ? "Showing All USERS!! (check the url)"
+      : "Showing Active USERS Only!! (check the url)";
   return (
     <div>
+      <pre>-------------------------------</pre>
+      <br />
+      <h3>
+        <em>{message}</em>
+      </h3>
+      <pre>-------------------------------</pre>
+      <button onClick={() => setSearchParams({ filter: "active" })}>
+        Show Active Users
+      </button>
+      <span>&nbsp;&nbsp;&nbsp;</span>
+      <button onClick={() => setSearchParams({})}>Remove Filters</button>
       {data.map((item) => {
         return (
-          <Link to={`${item.userId}`}>
-            <h1>{item.name}</h1>
-          </Link>
+          <div>
+            <Link to={`${item.userId}`}>
+              {status === "active" ? (
+                item.status === "active" ? (
+                  <h1>{item.name}</h1>
+                ) : (
+                  <></>
+                )
+              ) : (
+                <h1>{item.name}</h1>
+              )}
+            </Link>
+          </div>
         );
       })}
     </div>
